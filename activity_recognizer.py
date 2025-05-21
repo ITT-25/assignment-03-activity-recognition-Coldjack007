8d10d54dcd75aaa661ccb52d4ed5ef8120f5a6cf
Coldjack007 <69972794+Coldjack007@users.noreply.github.com>
2025-05-21 22:07:31 +0200

    Completed the fitness trainer.

--- activity_recognizer.py
+++ activity_recognizer.py
@@ -1,1 +1,116 @@
 # this program recognizes activities
+import os
+from glob import glob
+import seaborn as sns
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import scale, MinMaxScaler
+from sklearn import svm
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, confusion_matrix, ConfusionMatrixDisplay, classification_report
+import matplotlib.pyplot as plt
+from joblib import dump, load
+
+DATA_PATH = "data"
+MODEL_PATH = "svm_model.joblib"
+
+x_train = None
+x_test = None
+y_train = None
+y_test = None
+
+#Load all the datasets, phew
+def load_data():
+    all_datasets = []
+    for activity in os.listdir(DATA_PATH):
+        current_activity = os.path.join(DATA_PATH, activity)
+        for files in glob(f"{current_activity}/*.csv"):
+            df = pd.read_csv(files, on_bad_lines='skip')
+            df['activity'] = activity
+            all_datasets.append(df)
+    print("Data loaded.")
+    return pd.concat(all_datasets, ignore_index=True)
+
+def pre_process_data(df):
+    #filter out id, turn activities into numbers
+    #[['timestamp', 'acc_x', 'acc_y', 'acc_z', 'gyro_x', 'gyro_y', 'gyro_z']]
+    df['activity_num'] = 0
+    df.loc[df['activity'] == 'lifting', 'activity_num'] = 1
+    df.loc[df['activity'] == 'running', 'activity_num'] = 2
+    df.loc[df['activity'] == 'rowing', 'activity_num'] = 3
+    df = df.dropna()
+    filtered_df = df[['timestamp', 'acc_x', 'acc_y', 'acc_z', 'activity_num']]
+    
+    #Mean Removal
+    scaled_df = scale(filtered_df[['acc_x', 'acc_y', 'acc_z']])
+    subset_mean = filtered_df.copy()
+    subset_mean[['acc_x', 'acc_y', 'acc_z']] = scaled_df
+    #Normalization
+    scaler = MinMaxScaler()
+    scaler.fit(subset_mean[['acc_x', 'acc_y', 'acc_z']])
+    scaled_samples = scaler.transform(subset_mean[['acc_x', 'acc_y', 'acc_z']])
+    df_normalized = subset_mean.copy()
+    df_normalized[['acc_x', 'acc_y', 'acc_z']] = scaled_df
+
+    print("Data preprocessed.")
+    return df_normalized
+
+#Visualizing the data, I could see that the acc data seems easy-ish to distinguish, while the gyro seems not.
+#Thus, I will try to omit the gyro data for increased performance, hoping that the acc data alone is good enough.
+#Also, I have problems with DIPPID data that has been put into Arrays being stale when accessed from another method.
+#I will have asked about this on Thursday, unless I forgot. If I did, please write me a mail or contact me on Discord!
+def visualize_data(df):
+    sns.pairplot(df, hue='activity_num', vars=['acc_x', 'acc_y', 'acc_z', 'gyro_x', 'gyro_y', 'gyro_z'])
+    plt.show()
+
+def split_data(df):
+    global x_train, x_test, y_train, y_test
+    x = df[['acc_x', 'acc_y', 'acc_z']]
+    y = df['activity_num']
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, stratify=y)
+    print("Data split.")
+
+
+def train_model(df):
+    #poly=75%avg,  rbf=83%avg,   sigmoid=48%avg,   linear=71%avg   (mit allen 3 Achsen bei acc)
+    #poly=51%avg,  rbf=72%avg,   sigmoid=35%avg,   linear=61%avg   (mit y und z Achse bei acc)
+    classifier = svm.SVC(kernel='rbf')
+    classifier.fit(x_train, y_train)
+    return classifier
+
+def test_accuracy(clf):
+    print("Currently evaluating test data, please wait a bit...")
+    y_pred = clf.predict(x_test)
+    print("Accuracy value: ", accuracy_score(y_test, y_pred))
+
+
+activity_df = load_data()
+processed_df = pre_process_data(activity_df)
+#print(processed_df)
+#visualize_data(processed_df)
+split_data(processed_df)
+
+#model = train_model(processed_df)
+#dump(model, MODEL_PATH)
+model = load(MODEL_PATH)
+print("Model loaded.")
+test_accuracy(model)
+
+
+def evaluate_data(acc_data: pd.DataFrame):
+    #Mean removal
+    scaled_acc_data = scale(acc_data[['acc_x', 'acc_y', 'acc_z']])
+    acc_data_mean = acc_data.copy()
+    acc_data_mean[['acc_x', 'acc_y', 'acc_z']] = scaled_acc_data
+    #Normalization
+    scaler = MinMaxScaler()
+    scaler.fit(acc_data_mean[['acc_x', 'acc_y', 'acc_z']])
+    scaled_samples = scaler.transform(acc_data_mean[['acc_x', 'acc_y', 'acc_z']])
+    acc_data_normalized = acc_data_mean.copy()
+    acc_data_normalized[['acc_x', 'acc_y', 'acc_z']] = scaled_acc_data
+    
+    prediction = model.predict(acc_data_normalized)
+    return prediction
+
+
+    
--- fitness_trainer.py
+++ fitness_trainer.py
@@ -1,4 +1,124 @@
 # this program visualizes activities with pyglet
-
 import activity_recognizer as activity
+import os
+import numpy as np
+import pandas as pd
+import time
 import pyglet
+import DIPPID
+import random
+
+PORT = 5700
+sensor = DIPPID.SensorUDP(PORT)
+
+WINDOW_WIDTH = 800
+WINDOW_HEIGHT = 600
+
+win = pyglet.window.Window(WINDOW_WIDTH, WINDOW_HEIGHT)
+
+is_mainMenu = True
+is_Training = False
+game_quit_message = pyglet.text.Label("Press P to return to the menu and Q to quit the game.", WINDOW_WIDTH/2, WINDOW_HEIGHT-10, anchor_x="center", anchor_y="center", font_name="Times New Roman", font_size=14)
+mainMenu_message1 = pyglet.text.Label("Press Q to quit the game.", WINDOW_WIDTH/2, WINDOW_HEIGHT-10, anchor_x="center", anchor_y="center", font_name="Times New Roman", font_size=14)
+mainMenu_message2 = pyglet.text.Label("Press Button 1 on your DIPPID device to start!", WINDOW_WIDTH/2, WINDOW_HEIGHT/2, anchor_x="center", anchor_y="center", font_name="Times New Roman", font_size=26)
+
+exercise_msg1 = pyglet.text.Label("Current exercise: ", WINDOW_WIDTH/2, WINDOW_HEIGHT/2, anchor_x="center", anchor_y="center", font_name="Times New Roman", font_size=26)
+exercise_msg2 = pyglet.text.Label("0 out of 3 reps done!", WINDOW_WIDTH/2, WINDOW_HEIGHT/2-30, anchor_x="center", anchor_y="center", font_name="Times New Roman", font_size=12)
+
+all_activities = ['lifting', 'rowing', 'running', 'jumpingjacks']
+current_activity = ""
+current_activity_num = 0
+
+current_score = 0
+MAX_SCORE = 3
+confidence = 0
+CONFIDENCE_THRESHOLD = 100
+
+current_x_data = []
+current_y_data = []
+current_z_data = []
+DATA_COLLECTION_FREQUENCY = 1/100
+current_time = time.time()
+
+def get_next_activity():
+    global current_activity, current_activity_num
+    activity_num = random.randint(0,3)
+    current_activity = all_activities[activity_num]
+    current_activity_num = activity_num
+
+def init_game():
+    get_next_activity()
+
+def start_game(data):
+    global is_Training, is_mainMenu
+    if is_mainMenu is False:
+        pass
+    is_Training = True
+    is_mainMenu = False
+
+def handle_data(data):
+    global current_time, confidence, current_score
+    if is_Training is True:
+        if current_time - DATA_COLLECTION_FREQUENCY < time.time():
+            current_time = time.time()
+            current_x_data.append(data['x'])
+            current_y_data.append(data['y'])
+            current_z_data.append(data['z'])
+            if len(current_x_data) == 200:
+                full_data = np.array([current_x_data, current_y_data, current_z_data])
+                pd_data = pd.DataFrame(full_data).T
+                pd_data.columns = ['acc_x', 'acc_y', 'acc_z']
+                answer = activity.evaluate_data(pd_data)
+                print(answer)
+                for evaL_answer in range(len(answer)):
+                    if answer[evaL_answer] == current_activity_num:
+                        confidence += 1
+                if confidence >= CONFIDENCE_THRESHOLD:
+                    current_score += 1
+                if current_score == MAX_SCORE:
+                    get_next_activity()
+                    current_score = 0
+                print("Confidence: " + str(confidence))
+                confidence = 0
+                current_x_data.clear()
+                current_y_data.clear()
+                current_z_data.clear()
+
+def update_ui():
+    exercise_msg1.text = "Current exercise: " + all_activities[current_activity_num]
+    exercise_msg2.text = str(current_score) + " out of " + str(MAX_SCORE) + " done!"
+
+def exit_game():
+    global is_Training, is_mainMenu
+    if is_mainMenu is True:
+        pass
+    is_Training = False
+    is_mainMenu = True
+
+sensor.register_callback("button_1", start_game)
+sensor.register_callback("accelerometer", handle_data)
+init_game()
+
+@win.event
+def on_key_press(symbol, modifiers):
+    if symbol == pyglet.window.key.Q:
+        os._exit(0)
+    if symbol == pyglet.window.key.P:
+        exit_game()
+
+@win.event
+def on_draw():
+    win.clear()
+    if is_mainMenu:
+        mainMenu_message1.draw()
+        mainMenu_message2.draw()
+    elif is_Training:
+        update_ui()
+        game_quit_message.draw()
+        exercise_msg1.draw()
+        exercise_msg2.draw()
+
+pyglet.app.run()
+
+while True:
+    time.sleep(0.001)
